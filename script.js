98496da270e28a5cb4ed1d08257bb1a81f554695
27achang <179789901+27achang@users.noreply.github.com>
2026-01-13 18:44:42 -0800

    Prototype with Date class

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var running = false;
-var minutes = 59;
-var seconds = 58;
-var original_start_time = null;
+var pre_pause_milsecs = 0;
+var minutes = 0;
+var seconds = 0;
+var milliseconds = 0;
 var start_time = null;
 var stop_time = null;
 var reset_hold = false;
@@ -13,14 +14,11 @@ function start() {
         reset_hold = false;
         return;
     }
-    if (start_time == null) {
-        original_start_time = new Date();
-        document.querySelector("button").removeAttribute("disabled");
-    }
+    if (start_time == null) document.querySelector("button").removeAttribute("disabled");
     start_time = new Date();
     running = true;
-    update_interval = setInterval(updateStopwatch, 250);
-    document.querySelector("body").style.backgroundColor = "lime";
+    update_interval = setInterval(updateStopwatch, 1005);
+    document.querySelector("body").style.backgroundColor = "rgba(0, 255, 0, 0.3)";
     clear_background_interval = setInterval(clearBackground, 50);
 }
 
@@ -33,14 +31,9 @@ function pause() {
     running = false;
     stop_time = new Date();
     clearInterval(update_interval);
-    document.querySelector("body").style.backgroundColor = "red";
+    document.querySelector("body").style.backgroundColor = "rgba(255, 0, 0, 0.3)";
     clear_background_interval = setInterval(clearBackground, 50);
-    /*
-    let diff = getTimeDiff(new Date(), start_time);
-    hours += diff[0];
-    minutes += diff[1];
-    seconds += diff[2];
-    */
+    pre_pause_milsecs += Date.now() - start_time.getTime();
 }
 
 function toggleRun() {
@@ -50,50 +43,62 @@ function toggleRun() {
 
 function reset() {
     reset_hold = true;
-    original_start_time = start_time = stop_time = null;
-    hours = minutes = seconds = 0;
-    if (running) {
-        console.log("stopping");
-        clearInterval(update_interval);
-    }
+    start_time = stop_time = null;
+    pre_pause_milsecs = minutes = seconds = 0;
+    if (running) clearInterval(update_interval);
     running = false;
     document.querySelector("h1").textContent = "0:00";
     document.querySelector("button").setAttribute("disabled", "true");
 }
 
 function updateStopwatch() {
+    /*
     if ((minutes < 60 && seconds + 1 == 60) || (minutes >= 60 && seconds == minutes)) {
         minutes += 1;
         seconds = 0;
     } else seconds += 1;
     document.querySelector("h1").textContent = minutes + ":" + (seconds.toString().length == 1 ? "0" + seconds.toString() : seconds);
-    /*
-    let diff = getTimeDiff(new Date(), start_time);
-    document.querySelector("h1").textContent = styleTime(diff[0], diff[1], diff[2] + hours * 3600 + minutes * 60 + seconds);
-    
     */
-    // console.log("Hours: " + hours + "\nMinutes: " + minutes + "\nSeconds: " + seconds);
-    console.log("Minutes: " + minutes + "\nSeconds: " + seconds);
+    let ms = Date.now() - start_time.getTime() + pre_pause_milsecs;
+    if (Math.trunc(ms / 60000) <= 60) {
+        ms -= ms % 1000;
+        minutes = Math.trunc(ms / 60000);
+        seconds = (ms - minutes * 60000) / 1000;
+    } else if ((60 + Math.trunc(ms / 1000) % 60) < Math.trunc(ms / 60000)) {
+        ms -= ms % 1000;
+        minutes = Math.trunc(ms / 60000) - 1;
+        seconds = (ms - minutes * 60000) / 1000;
+    } else {
+        ms -= ms % 1000;
+        minutes = Math.trunc(ms / 60000);
+        seconds = (ms - minutes * 60000) / 1000;
+    }
+    document.querySelector("h1").textContent =  minutes + ":" + (seconds < 10 ? "0" + seconds.toString() : seconds);
 }
 
 window.onload = (e) => {
     document.querySelector("div").style.left = "calc(50% - " + document.querySelector("div").offsetWidth / 2 + "px";
 }
 
+/*
 function getTimeDiff(time1, time2) {
     let ms_diff = Math.abs(time1.getTime() - time2.getTime());
     let remainder = ms_diff;
-    let hr_diff = Math.trunc(ms_diff / 3600000);
-    remainder -= hr_diff * 3600000;
     let min_diff = Math.trunc(remainder / 60000);
     remainder -= min_diff * 60000;
     let sec_diff = Math.trunc(remainder / 1000);
-    return [hr_diff, min_diff, sec_diff];
+    return [min_diff, sec_diff];
     if (hr_diff > 0) return hr_diff + ":" + (min_diff.toString().length == 1 ? "0" + min_diff.toString() : min_diff) + ":" + (sec_diff.toString().length == 1 ? "0" + sec_diff.toString() : sec_diff);
     else return min_diff + ":" + (sec_diff.toString().length == 1 ? "0" + sec_diff.toString() : sec_diff);
 }
 
-function styleTime(hr_input, min_input, sec_input) {
+function styleTime(sec_input) {
+    if (sec_input % 60 <= 60) {
+        let min = Math.trunc(sec_input / 60);
+        let sec = sec_input % 60;
+        return min + ":" + (sec.toString().length == 1 ? "0" + sec.toString() : sec);
+    }
+    /*
     let remainder = sec_input + hr_input * 3600 + min_input * 60;
     let hr = Math.trunc(remainder / 3600);
     remainder -= hr * 3600;
@@ -102,4 +107,6 @@ function styleTime(hr_input, min_input, sec_input) {
     let sec = remainder;
     if (hr > 0) return hr + ":" + (min.toString().length == 1 ? "0" + min.toString() : min) + ":" + (sec.toString().length == 1 ? "0" + sec.toString() : sec);
     else return min + ":" + (sec.toString().length == 1 ? "0" + sec.toString() : sec);
+    *//*
 }
+*/
